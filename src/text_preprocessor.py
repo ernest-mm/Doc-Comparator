e694a6094d1b00577ddb6cf069d235f0cef55608
Ernest M. <180271155+ernest-mm@users.noreply.github.com>
2025-06-08 15:52:23 +0200

    Prototype de la fonction convert_to_lowercase

---
+++ src/text_preprocessor.py
@@ -0,0 +1,12 @@
+import re
+import string
+
+def convert_to_lowercase(text: str) -> str:
+    """
+    Convertit l'intégralité du texte en minuscules.
+    
+    :param text: Texte d'origine.
+    :return: Texte converti en minuscules.
+    """
+    #TODO: Implémenter la conversion en minuscules
+
